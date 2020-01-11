1ed038b5a454c075fe72f423ed549ddb2a9af611
MimiMicek <makaveli_girl1996@hotmail.com>
2020-01-11 15:28:01 +0100

    ´homepage done

--- client/src/components/home/HomePage.js
+++ client/src/components/home/HomePage.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import library from './library.jpg';
+import './HomePage.css';
 
 export default class HomePage extends React.Component{
   render(){
     return(
-        <h1>Welcome to the Home page</h1>
+      <div className="container">
+        <div className="jumbotron jumbotron-fluid mt-5">
+          <div className="col-sm-8 mx-auto">
+            <h1 className="text-center">Welcome to online library</h1>
+            <img className="img-fluid" src={library}></img>
+          </div>
+        </div>
+      </div>
     );
   }
 }
